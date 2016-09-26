b3b05c72dbf9f7bcbe74d7f3554097ff8aebfcd0
Ruthvik Vijayakumar <coderuth@outlook.com>
2016-09-26 15:33:37 +0530

    hanoi added

---
+++ towerOfHanoi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int hanoi(int num,char from,char to,char aux);
+
+main() {
+    int num;
+    printf("Enter the Number of Disks:\n");
+    scanf("%d",&num);
+    if(num<=0)
+        printf("Enter more than 0 \n");
+    else {
+        printf("The Sequence Involved is:\n");
+        hanoi(num,'A','C','B');
+    }
+}
+
+int hanoi(int num,char from,char to,char aux) {
+    if(num==1) {
+        printf("Moving Disk from %c to %c \n",from,to);
+    }
+    else {
+        hanoi(num-1,from,aux,to);
+
+        printf("Moving %d Disk from %c to %c \n",num,from,to);
+
+        hanoi(num-1,aux,to,from);
+    }
+}
